3db4b7166a77f2b89878725f962ba2e8ad050e76
Bankhardt Mate <bankhardtmate@gmail.com>
2024-10-18 12:48:54 +0200

    ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbankhar <mbankhar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 17:45:09 by mbankhar          #+#    #+#             */
+/*   Updated: 2024/10/17 10:56:43 by mbankhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int main( void ) {
+Fixed a;
+Fixed b( a );
+Fixed c;
+c = b;
+std::cout << a.getRawBits() << std::endl;
+std::cout << b.getRawBits() << std::endl;
+std::cout << c.getRawBits() << std::endl;
+return 0;
+}
