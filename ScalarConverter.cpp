1b634c71ec26177113598d72cd681dcd7be6c583
Bankhardt Mate <bankhardtmate@gmail.com>
2025-01-13 10:19:06 +0100

    ex00

---
+++ ScalarConverter.cpp
@@ -0,0 +1,25 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{
+
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& other)
+{
+
+}
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
+{
+    return *this;
+}
+
+ScalarConverter::~ScalarConverter()
+{
+
+}
+
+void ScalarConverter::convert(const std::string& literal) {
+   
+}
