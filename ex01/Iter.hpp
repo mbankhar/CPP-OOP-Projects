5ae2a0407c129da168ee07816984f32832ee98e2
Bankhardt Mate <bankhardtmate@gmail.com>
2025-01-26 14:29:10 +0100

    ex01

---
+++ ex01/Iter.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+template <typename T, typename Func>
+void iter(T* array, size_t length, Func func)
+{
+	for (size_t i = 0; i < length; ++i)
+	{
+		func(array[i]);
+	}
+}
