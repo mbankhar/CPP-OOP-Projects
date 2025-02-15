1f2a9d1f462618c164303bd85bafd6fa07fcdb2a
Bankhardt Mate <bankhardtmate@gmail.com>
2025-02-15 16:33:19 +0100

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,27 @@
+#include "PmergeMe.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		std::cerr << "Error" << std::endl;
+		return 1;
+	}
+
+	try
+	{
+		PmergeMe pm;
+		pm.parseArguments(argc, argv);
+		pm.printBefore();
+		pm.sortContainers();
+		pm.printAfter();
+		pm.printTimes();
+	catch (const std::exception& e)
+	{
+		std::cerr << "Error" << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
